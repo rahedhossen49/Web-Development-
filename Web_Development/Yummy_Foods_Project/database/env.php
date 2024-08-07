b17f8c2ef487fa7a9cea5c8f14193739f0bbf51c
rahedhossen49 <rahedhossen49@gmail.com>
2024-08-08 00:58:18 +0600

    update

---
+++ Web_Development/Yummy_Foods_Project/database/env.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$DatabaseHostName = 'localhost';
+$DatabaseUserName = 'root';
+$DatabasePassword = '';
+$DatabaseName = 'yummy_food';
+
+
+$connection = mysqli_connect($DatabaseHostName,$DatabaseUserName,$DatabasePassword,$DatabaseName);
+
+
+if (!$connection) {
+ die('Could not connect: ' . mysqli_connect_error());
+}
+
+
+
+
+
+
+
+
+?>
